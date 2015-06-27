26c2d44fe6a94d387a0417c6b5afc13f7e543875
raozijian <sikenrao@gmail.com>
2015-06-27 16:19:47 +0800

    重做一版

--- src/resource.js
+++ src/resource.js
@@ -1,43 +1,24 @@
 var res = {
-    ROLE_1 : "res/roles/1.png",
-    ROLE_2 : "res/roles/2.png",
-    ROLE_3 : "res/roles/3.png",
-    ROLE_4 : "res/roles/4.png",
-    ROLE_5 : "res/roles/5.png",
-    ROLE_6 : "res/roles/6.png",
-    ROLE_7 : "res/roles/7.png",
-    ROLE_8 : "res/roles/8.png",
-    ROLE_9 : "res/roles/9.png",
-    ROLE_10 : "res/roles/10.png",
-    ROLE_11 : "res/roles/11.png",
-    ROLE_12 : "res/roles/12.png",
-    ROLE_13 : "res/roles/13.png",
-    ROLE_14 : "res/roles/14.png",
-    ROLE_15 : "res/roles/15.png",
-    ROLE_16 : "res/roles/16.png",
-
-    Btn_Start_Normal:"res/btn_start_normal.png",
-    Btn_Start_Push:"res/btn_start_push.png",
-
-    Btn_pressed_animal_1:"res/btn_pressed_animal_1.png",
-    Btn_normal_animal_1:"res/btn_normal_animal_1.png",
-    Btn_pressed_animal_2:"res/btn_pressed_animal_2.png",
-    Btn_normal_animal_2:"res/btn_normal_animal_2.png",
-    Btn_pressed_animal_3:"res/btn_pressed_animal_3.png",
-    Btn_normal_animal_3:"res/btn_normal_animal_3.png",
-    Btn_pressed_animal_4:"res/btn_pressed_animal_4.png",
-    Btn_normal_animal_4:"res/btn_normal_animal_4.png",
-
-    Btn_normal_count_7:"res/btn_normal_count_7.png",
-    Btn_pressed_count_7:"res/btn_pressed_count_7.png",
-    Btn_normal_push_3:"res/btn_normal_push_3.png",
-    Btn_normal_plus:"res/btn_normal_plus.png",
-    Btn_normal_count_people:"res/btn_normal_count_people.png",
-    Btn_pressed_count_people:"res/btn_pressed_count_people.png",
-    Btn_pressed_plus:"res/btn_pressed_plus.png",
-    Btn_pressed_push_3:"res/btn_pressed_push_3.png",
-
-
+    //Btn_Start_Normal:"res/btn_start_normal.png",
+    //Btn_Start_Push:"res/btn_start_push.png",
+    //
+    //Btn_pressed_animal_1:"res/btn_pressed_animal_1.png",
+    //Btn_normal_animal_1:"res/btn_normal_animal_1.png",
+    //Btn_pressed_animal_2:"res/btn_pressed_animal_2.png",
+    //Btn_normal_animal_2:"res/btn_normal_animal_2.png",
+    //Btn_pressed_animal_3:"res/btn_pressed_animal_3.png",
+    //Btn_normal_animal_3:"res/btn_normal_animal_3.png",
+    //Btn_pressed_animal_4:"res/btn_pressed_animal_4.png",
+    //Btn_normal_animal_4:"res/btn_normal_animal_4.png",
+    //
+    //Btn_normal_count_7:"res/btn_normal_count_7.png",
+    //Btn_pressed_count_7:"res/btn_pressed_count_7.png",
+    //Btn_normal_push_3:"res/btn_normal_push_3.png",
+    //Btn_normal_plus:"res/btn_normal_plus.png",
+    //Btn_normal_count_people:"res/btn_normal_count_people.png",
+    //Btn_pressed_count_people:"res/btn_pressed_count_people.png",
+    //Btn_pressed_plus:"res/btn_pressed_plus.png",
+    //Btn_pressed_push_3:"res/btn_pressed_push_3.png",
     Number_1: "res/numbers/1.png",
     Number_2: "res/numbers/2.png",
     Number_3: "res/numbers/3.png",
@@ -50,10 +31,10 @@ var res = {
     Number_0: "res/numbers/0.png",
 
 
-    Logo:"res/logo.png",
-    board:"res/board.png",
-
-    Audio_BGM:"res/BGM/RoyalFlushParty.mp3"
+    //
+    //
+    //Logo:"res/logo.png",
+    //board:"res/board.png"
 
 };
 
