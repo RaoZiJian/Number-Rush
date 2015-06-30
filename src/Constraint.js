dcd969beafa07dd21ff61eff6f2478b1d54e5c44
raozijian <sikenrao@gmail.com>
2015-06-30 16:04:13 +0800

    count 7

--- src/Constraint.js
+++ src/Constraint.js
@@ -3,7 +3,9 @@
  */
 var Game_Constraint = {
 
-    //选择场景各个role之间的间距
-    ChooseRoleOffset: 120
+    //The names of count 7
+    Count7Names:["汪汪","呆鸡","YOU","桑桑","超人","傻喵","阿花","建建"],
 
+    //The positions of count7
+    Count7Positions:[cc.p(0,280),cc.p(180,220),cc.p(240,40),cc.p(160,-140),cc.p(0,-200),cc.p(-160,-140),cc.p(-240,40),cc.p(-180,220)]
 }
--- src/count7.js
+++ src/count7.js
@@ -2,7 +2,60 @@
  * Created by Kirito on 6/29/15.
  */
 var MainScene;
+
+var Count7Player = cc.Sprite.extend({
+
+    _name:null,
+    _nameLabel:null,
+    _sequenceId: null,
+    _isMe: false,
+
+    ctor: function (isMe,sequenceId) {
+        this._super()
+        this._isMe = isMe;
+        this._sequenceId = sequenceId;
+
+        if(isMe){
+            this.initWithSpriteFrameName("Shu7_0004.png");
+        }else{
+            this.initWithSpriteFrameName("Shu7_0003.png");
+        }
+
+        this._name = Game_Constraint.Count7Names[sequenceId];
+        this._nameLabel = new cc.LabelTTF(this._name);
+        this._nameLabel.setFontFillColor(cc.color(255,237,191));
+        this._nameLabel.setFontSize(30);
+        this.addChild(this._nameLabel);
+    },
+
+    getName: function () {
+        return this._name;
+    },
+
+    checkIsMe:function(){
+        return this._isMe;
+    },
+
+    init: function () {
+        if(!this._super()){
+            return false;
+        };
+
+        return true;
+    },
+
+    onEnter: function () {
+        this._super();
+
+    }
+});
+
 var Count7Layer = cc.Layer.extend({
+
+    players:[],
+    catBg:null,
+    selfPlayer:null,
+
     ctor:function(){
         this._super();
     },
@@ -13,6 +66,30 @@ var Count7Layer = cc.Layer.extend({
         }
 
         return true;
+    },
+
+    initPlayers:function(){
+
+        var winSize = cc.director.getWinSize();
+        for(var i=0;i<8;i++){
+            var player = new Count7Player(false, i);
+            if(2==i) {
+                player = new Count7Player(true, i);
+            }
+            player.setPosition(Game_Constraint.Count7Positions[i].x+winSize.width/2,Game_Constraint.Count7Positions[i].y+winSize.height/2);
+            player.setScale(0)
+            var catDelay = cc.delayTime(2.8+i*0.2);
+            var catAct1 = cc.scaleTo(0.3,1.15);
+            var catAct2 = cc.scaleTo(0.1,1);
+            var catAct3 = cc.scaleTo(0.1,1.1);
+            var catAct6 = cc.scaleTo(0.1,1);
+            var catAct7 = cc.scaleTo(0.1,1.05);
+            var catAct8 = cc.scaleTo(0.1,1);
+            var catAct9 = cc.sequence(catDelay,catAct1,catAct2,catAct3,catAct6,catAct7,catAct8);
+            player.runAction(catAct9);
+            this.addChild(player);
+            this.players.push(player);
+        }
     },
 
     btnBackBtnTouchEvent: function (sender, type) {
@@ -75,15 +152,21 @@ var Count7Layer = cc.Layer.extend({
         this.addChild(count7Logo);
 
         //cat
-        var cat = new cc.Sprite(res.Count7Cat);
-        cat.setPosition(bg.getContentSize().width/2,bg.getContentSize().height/2);
-        //cat.setScale(0);
-        //var catDelay = cc.delayTime(1.6);
-        //var catAct1 = cc.scaleTo(0.4,1.1);
-        //var catAct2 = cc.scaleTo(0.2,1);
-        //var catAct3 = cc.sequence(catDelay,catAct1,catAct2);
-        //cat.runAction(catAct3);
-        this.addChild(cat);
+        this.catBg = new cc.Sprite(res.Count7Cat);
+        this.catBg.setPosition(bg.getContentSize().width/2,bg.getContentSize().height/2);
+        this.catBg.setScale(0);
+        var catDelay = cc.delayTime(1.6);
+        var catAct1 = cc.scaleTo(0.3,1.15);
+        var catAct2 = cc.scaleTo(0.1,1);
+        var catAct3 = cc.scaleTo(0.1,1.1);
+        var catAct6 = cc.scaleTo(0.1,1);
+        var catAct7 = cc.scaleTo(0.1,1.05);
+        var catAct8 = cc.scaleTo(0.1,1);
+        var catAct9 = cc.sequence(catDelay,catAct1,catAct2,catAct3,catAct6,catAct7,catAct8);
+        this.catBg.runAction(catAct9);
+        this.addChild(this.catBg);
+
+        this.initPlayers();
     }
 });
 
